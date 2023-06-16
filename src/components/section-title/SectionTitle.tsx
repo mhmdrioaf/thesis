b125a127c4317bdc900db9f42b1274404bbd9872
Rio Ananta <rioananta1@gmail.com>
2023-06-16 20:27:31 +0700

    `modify:` section title component

--- src/components/section-title/SectionTitle.tsx
+++ src/components/section-title/SectionTitle.tsx
@@ -2,7 +2,7 @@ export default function SectionTitle({ title }: { title: string }) {
   return (
     <div className="w-48 flex flex-col gap-2">
       <h3 className="font-bold text-xl">{title}</h3>
-      <div className="bg-primary w-1/4 h-1" />
+      <div className="bg-primary w-1/4 h-1 rounded-lg" />
     </div>
   );
 }
