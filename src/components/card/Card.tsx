64d2a1e76dafae4913f6eb0a17b515de8d1c2469
Rio Ananta <rioananta1@gmail.com>
2023-06-18 10:09:27 +0700

    `component` card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,23 +1,7 @@
-import Image from "next/image";
-
-export default function Card({
-  img,
-  title,
-  desc,
-}: {
-  img: string;
-  title: string;
-  desc: string;
-}) {
+export default function Card({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full h-full rounded-lg select-none cursor-pointer border border-gray-400 flex flex-col gap-4 relative items-center overflow-hidden">
-      <div className="w-screen h-96 relative">
-        <Image fill src={img} alt="Major" className="object-cover" />
-      </div>
-      <div className="px-4 w-full mb-4 flex flex-col">
-        <h3 className="text-2xl font-bold">{title}</h3>
-        <p>{desc}</p>
-      </div>
+    <div className="rounded-lg select-none cursor-pointer border border-gray-400 flex flex-col gap-4 relative items-center overflow-hidden">
+      {children}
     </div>
   );
 }
