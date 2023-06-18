008a0cfd80d45e82cce8d97dca1869d95ada3f90
Rio Ananta <rioananta1@gmail.com>
2023-06-18 14:29:07 +0700

    `pages` login page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,19 @@
+import Card from "@/components/card/Card";
+import Container from "@/components/container/Container";
+import AuthForm from "@/components/forms/AuthForm";
+
+export default function Login() {
+  return (
+    <Container className="w-full min-h-screen flex justify-center items-center">
+      <Card className="w-full lg:w-2/3 px-8 py-8 border-gray-100 cursor-auto gap-8">
+        <div className="w-full flex flex-col gap-2">
+          <p className="text-2xl text-primary font-bold">Login</p>
+          <p className="text-neutral-500">
+            Please enter your email or username and password to log in.
+          </p>
+        </div>
+        <AuthForm />
+      </Card>
+    </Container>
+  );
+}
