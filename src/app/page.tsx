080ccc2ccabc33e5e2e9aad157985a22e80c20ad
Rio Ananta <rioananta1@gmail.com>
2023-06-18 11:49:37 +0700

    `refactor` dummy assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,12 @@ import Carousel from "@/components/carousel/Carousel";
 import Container from "@/components/container/Container";
 import SectionTitle from "@/components/section-title/SectionTitle";
 import Speech from "@/components/speech/Speech";
+import { DUMMY_HOME_CAROUSEL_ASSETS as dummyCarouselAssets } from "@/lib/constants";
 
 export default function Home() {
-  const dummyCarouselAssets: CarouselAsset[] = [
-    {
-      id: "ca-1",
-      img: "/carousel_1.jpg",
-      desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ipsum voluptatem provident.`,
-      title: `Lorem ipsum dolor sit.`,
-    },
-    {
-      id: "ca-2",
-      img: "/carousel_2.jpg",
-      desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ipsum voluptatem provident.`,
-      title: `Lorem ipsum dolor sit.`,
-    },
-    {
-      id: "ca-3",
-      img: "/carousel_3.jpg",
-      desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ipsum voluptatem provident.`,
-      title: `Lorem ipsum dolor sit.`,
-    },
-  ];
-
   return (
     <main>
-      <Carousel assets={dummyCarouselAssets} />
+      <Carousel assets={dummyCarouselAssets} className="h-64 lg:h-screen" />
       <Speech />
       <Container className="flex flex-col gap-8">
         <SectionTitle title="Available Majors" />
