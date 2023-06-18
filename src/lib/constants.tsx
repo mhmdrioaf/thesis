9fa9364b2bae4804da1ab25174d0d236c32a5fa6
Rio Ananta <rioananta1@gmail.com>
2023-06-18 11:48:51 +0700

    `assets` dummy assets

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -19,6 +19,83 @@ const socialMediaData = {
   },
 };
 
+export const DUMMY_HOME_CAROUSEL_ASSETS: CarouselAsset[] = [
+  {
+    id: "ca-1",
+    img: "/carousel_1.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ipsum voluptatem provident.`,
+    title: `Lorem ipsum dolor sit.`,
+  },
+  {
+    id: "ca-2",
+    img: "/carousel_2.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ipsum voluptatem provident.`,
+    title: `Lorem ipsum dolor sit.`,
+  },
+  {
+    id: "ca-3",
+    img: "/carousel_3.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ipsum voluptatem provident.`,
+    title: `Lorem ipsum dolor sit.`,
+  },
+];
+
+export const DUMMY_MARKETPLACE_CAROUSEL_ASSETS: CarouselAsset[] = [
+  {
+    id: "ca-1",
+    img: "/product-category-1.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur.`,
+    title: `Hortikultura`,
+  },
+  {
+    id: "ca-2",
+    img: "/product-category-2.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    title: `Lorem ipsum`,
+  },
+  {
+    id: "ca-3",
+    img: "/product-category-3.jpg",
+    desc: `Lorem ipsum dolor sit amet.`,
+    title: `Ruminansia`,
+  },
+];
+
+export const DUMMY_MARKETPLACE_PRODUCTS: Product[] = [
+  {
+    id: "prod01",
+    name: "Product 1",
+    price: 50000,
+    category: "Ruminansia",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum iure in accusantium quia voluptatum optio?`,
+    imgURL: "/product-category-1.jpg",
+  },
+  {
+    id: "prod02",
+    name: "Product 2",
+    price: 24000,
+    category: "Hortikultura",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum iure in accusantium quia voluptatum optio?`,
+    imgURL: "/product-category-2.jpg",
+  },
+  {
+    id: "prod03",
+    name: "Product 3",
+    price: 32000,
+    category: "Ruminansia",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum iure in accusantium quia voluptatum optio?`,
+    imgURL: "/product-category-3.jpg",
+  },
+  {
+    id: "prod04",
+    name: "Product 4",
+    price: 12500,
+    category: "Florikultura",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum iure in accusantium quia voluptatum optio?`,
+    imgURL: "/product-category-1.jpg",
+  },
+];
+
 export const HOME_TABS: Tab[] = [
   { id: "home", route: "/", name: "Home" },
   { id: "about", route: "/about", name: "About" },
