1bd182939f1378ad1cdff262d70dd0edbc4a0740
Rio Ananta <rioananta1@gmail.com>
2023-06-23 09:59:31 +0700

    `auth` using basic auth initialization

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -1,66 +1,31 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { NextAuthOptions } from "next-auth";
-import type { Adapter } from "next-auth/adapters";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { db } from "./db";
+import type { AuthOptions } from "next-auth/core/types";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(db) as Adapter,
-  secret: process.env.NEXTAUTH_SECRET,
+export const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
-      name: "Credentials",
       credentials: {
         username: { type: "text" },
         password: { type: "text" },
       },
       async authorize(credentials) {
-        const res = await fetch("http://localhost:3000/api/auth/login", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        });
-        const user = await res.json();
+        const authResponse = await fetch(
+          "http://localhost:3000/api/auth/login",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(credentials),
+          }
+        );
 
-        if (res.ok && user) {
-          return user;
+        if (!authResponse.ok) {
+          return null;
         }
 
-        return null;
+        const user = await authResponse.json();
+
+        return user;
       },
     }),
   ],
-  session: {
-    strategy: "jwt",
-  },
-  callbacks: {
-    session({ token, session }) {
-      if (token) {
-        (session.user.id = token.id),
-          (session.user.email = token.email),
-          (session.user.name = token.name),
-          (session.user.image = token.picture);
-      }
-      return session;
-    },
-    async jwt({ token, user }) {
-      const dbUser = await db.user.findFirst({
-        where: {
-          email: token.email,
-        },
-      });
-
-      if (!dbUser) {
-        token.id = user.id;
-        return token;
-      }
-
-      return {
-        id: dbUser.id,
-        email: dbUser.email,
-        name: dbUser.name,
-        picture: dbUser.image,
-      };
-    },
-  },
 };
