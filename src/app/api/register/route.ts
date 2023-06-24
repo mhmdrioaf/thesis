98f604ecd5b34279872f24f7780b65415f3f5ea4
Rio Ananta <rioananta1@gmail.com>
2023-06-24 15:23:27 +0700

    `optimize` return correct error code for contraints violation

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import * as bcrypt from "bcrypt";
+import { Prisma } from "@prisma/client";
 
 interface RequestBody {
   username?: string;
@@ -12,17 +13,31 @@ interface RequestBody {
 const handler = async (request: NextRequest) => {
   const body: RequestBody = await request.json();
 
-  const newUser = await db.user.create({
-    data: {
-      username: body.username,
-      name: body.name,
-      email: body.email,
-      password: await bcrypt.hash(body.password, 10),
-    },
-  });
+  try {
+    const newUser = await db.user.create({
+      data: {
+        username: body.username,
+        name: body.name,
+        email: body.email,
+        password: await bcrypt.hash(body.password, 10),
+      },
+    });
 
-  const { password, ...result } = newUser;
-  return new NextResponse(JSON.stringify(result));
+    const { password, ...result } = newUser;
+    return new NextResponse(JSON.stringify(result));
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        return NextResponse.json(
+          { error: "Email or username are already registered." },
+          {
+            status: 409,
+            statusText: "Email or username are already registered",
+          }
+        );
+      }
+    }
+  }
 };
 
 export { handler as GET, handler as POST };
