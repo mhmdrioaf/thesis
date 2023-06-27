5afcee9750253942c20c4a3d0578904076f7a928
Rio Ananta <rioananta1@gmail.com>
2023-06-28 05:30:14 +0700

    `pages` user profile

---
+++ src/app/user/profile/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Container from "@/components/container/Container";
+import Addresses from "@/components/forms/(user)/Addresses";
+import PaymentMethod from "@/components/forms/(user)/PaymentMethod";
+import PersonalInfo from "@/components/forms/(user)/(PersonalInfo)/PersonalInfo";
+import Security from "@/components/forms/(user)/Security";
+import { PROFILE_PAGE_TABS } from "@/lib/constants";
+import { useState } from "react";
+
+export default function UserProfile() {
+  const [activePage, setActivePage] = useState<string | undefined>(
+    PROFILE_PAGE_TABS[0].id
+  );
+  const tabs = PROFILE_PAGE_TABS;
+
+  function activePageChecker(page: string | undefined) {
+    const defaultStyle = `w-full cursor-pointer uppercase font-semibold text-center border-b-4 transition-all ${
+      activePage === page ? "border-b-primary" : "border-b-transparent"
+    }`;
+
+    return defaultStyle;
+  }
+
+  function showActivePage(page: string | undefined) {
+    switch (page) {
+      case "bio":
+        return <PersonalInfo />;
+
+      case "addresses":
+        return <Addresses />;
+
+      case "payments":
+        return <PaymentMethod />;
+
+      case "security":
+        return <Security />;
+
+      default:
+        return (
+          <div className="flex flex-col gap-2">
+            <p className="text-gray-300">Oops...</p>
+            <p className="text-gray-500">Options not found</p>
+          </div>
+        );
+    }
+  }
+
+  return (
+    <Container className="flex flex-col gap-4">
+      <div className="flex flex-row gap-8 justify-evenly items-center border-b border-b-gray-300">
+        {tabs.map((tab: Tab) => (
+          <div
+            key={tab.id}
+            className={activePageChecker(tab.id)}
+            onClick={() => setActivePage(tab.id)}
+          >
+            {tab.name}
+          </div>
+        ))}
+      </div>
+      {showActivePage(activePage)}
+    </Container>
+  );
+}
