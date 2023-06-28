c531e1a4d2513caf0406bb821902b70a64cc89da
Rio Ananta <rioananta1@gmail.com>
2023-06-28 10:25:55 +0700

    `insert` username and phoneNumber to the session & token

--- src/lib/next-auth.d.ts
+++ src/lib/next-auth.d.ts
@@ -2,10 +2,14 @@ import type { Session, User } from "next-auth";
 import type { JWT } from "next-auth/jwt";
 
 type UserId = string;
+type UserUsername = string;
+type UserPhone = number;
 
 declare module "next-auth/jwt" {
   interface JWT {
     id: UserId;
+    phoneNumber?: UserPhone;
+    username: UserUsername;
   }
 }
 
@@ -13,6 +17,8 @@ declare module "next-auth" {
   interface Session {
     user: User & {
       id: UserId;
+      phoneNumber?: UserPhone;
+      username: UserUsername;
     };
   }
 }
