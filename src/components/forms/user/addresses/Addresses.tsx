37372ff5eeabe23781995c1153b8c3b348b4f544
Rio Ananta <rioananta1@gmail.com>
2023-06-30 20:54:45 +0700

    `insert` address CRUD

--- src/components/forms/user/addresses/Addresses.tsx
+++ src/components/forms/user/addresses/Addresses.tsx
@@ -4,13 +4,14 @@ import CardContainer from "@/components/container/CardContainer";
 import ModalsContainer from "@/components/container/ModalsContainer";
 import LoadingSpinner from "@/components/indicators/LoadingSpinner";
 import { CheckIcon, PlusIcon } from "@heroicons/react/24/solid";
-import { useEffect, useState } from "react";
+import { FormEvent, FormEventHandler, useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useSession } from "next-auth/react";
 import { phoneNumberConverter, sortAddress } from "@/lib/helper";
 
 type Inputs = {
   address: Address;
+  isDelete?: boolean;
 };
 
 interface User {
@@ -23,7 +24,11 @@ export default function Addresses() {
   const { register, handleSubmit, reset } = useForm<Inputs>();
   const { data: session, update, status } = useSession();
   const [modalShown, setModalShown] = useState<string | null>(null);
+  const [message, setMessage] = useState<string | null>(null);
   const [user, setUser] = useState<User | null>(null);
+  const [addressToUpdate, setAddressToUpdate] = useState<
+    Address | null | undefined
+  >(null);
   const [isLoading, setIsLoading] = useState(false);
 
   const userAddress = user?.addresses;
@@ -58,12 +63,45 @@ export default function Addresses() {
       console.error("An error occurred: ", err);
     }
   };
+  const onUpdate: SubmitHandler<Inputs> = async (data) => {
+    setIsLoading(true);
+    try {
+      const { id, ...dataToSubmit } = data.address;
+      const res = await fetch("http://localhost:3000/api/update-address", {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          isUpdate: true,
+          addressId: addressToUpdate?.id,
+          address: dataToSubmit,
+        }),
+      });
+
+      const updateResponse = await res.json();
+
+      if (!updateResponse.ok) {
+        console.error(updateResponse);
+      } else {
+        setAddressToUpdate(null);
+        setModalShown(null);
+        setIsLoading(false);
+        update();
+        reset();
+      }
+    } catch (err) {
+      setIsLoading(false);
+      console.error("An error occurred: ", err);
+    }
+  };
 
   function hideModal() {
     setModalShown(null);
     reset();
   }
-  function showModal(options: string | null) {
+  function showModal(
+    options: string | null,
+    address: Address | null | undefined
+  ) {
     switch (options) {
       case "add": {
         return (
@@ -125,6 +163,100 @@ export default function Addresses() {
           </ModalsContainer>
         );
       }
+      case "update": {
+        return (
+          <ModalsContainer
+            onClose={hideModal}
+            description="Update the current address"
+            title="Update Address"
+          >
+            <form
+              onSubmit={handleSubmit(onUpdate)}
+              className="flex flex-col gap-4"
+            >
+              <label htmlFor="label">Address Label</label>
+              <input
+                id="label"
+                className="px-2 py-2 border border-gray-300 rounded-md"
+                type="text"
+                defaultValue={address?.label}
+                {...register("address.label")}
+                required
+                placeholder="Home / Office"
+              />
+              <label htmlFor="receiverName">Recipient Name</label>
+              <input
+                id="receiverName"
+                className="px-2 py-2 border border-gray-300 rounded-md"
+                type="text"
+                defaultValue={address?.receiverName}
+                {...register("address.receiverName")}
+                required
+                placeholder="Recipent name..."
+              />
+              <label htmlFor="receiverPhone">Recipient Phone Number</label>
+              <input
+                id="receiverPhone"
+                className="px-2 py-2 border border-gray-300 rounded-md"
+                type="number"
+                defaultValue={address?.receiverPhone}
+                {...register("address.receiverPhone", {
+                  setValueAs: (v) => phoneNumberConverter(v),
+                })}
+                required
+                placeholder="08xx"
+              />
+              <label htmlFor="fullAddress">Full Address</label>
+              <textarea
+                id="fullAddress"
+                className="px-2 py-2 border border-gray-300 rounded-md"
+                rows={2}
+                defaultValue={address?.fullAddress}
+                {...register("address.fullAddress")}
+                placeholder="Jl..."
+                required
+              />
+              <button
+                type="submit"
+                className="w-full px-2 py-2 rounded-md bg-primary text-white disabled:bg-gray-300 disabled:text-gray-500"
+                disabled={isLoading}
+              >
+                Save
+              </button>
+            </form>
+          </ModalsContainer>
+        );
+      }
+      case "delete": {
+        return (
+          <ModalsContainer
+            title="Delete address"
+            description="Are you sure want to delete this address?"
+            onClose={hideModal}
+          >
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={(event) => deleteAddress(event, address!.id)}
+            >
+              <button
+                type="submit"
+                className="px-2 py-2 rounded-md bg-red-950 text-white disabled:bg-gray-300 disabled:text-gray-500"
+                disabled={isLoading}
+              >
+                Delete
+              </button>
+              <button
+                type="button"
+                className="px-2 py-2 rounded-md bg-neutral-500 text-neutral-50 disabled:bg-gray-300 disabled:text-gray-500"
+                disabled={isLoading}
+                onClick={hideModal}
+              >
+                Cancel
+              </button>
+            </form>
+          </ModalsContainer>
+        );
+      }
     }
   }
   async function updatePrimaryAddress(addressId: string) {
@@ -134,6 +266,7 @@ export default function Addresses() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
+          isUpdate: false,
           addressId: addressId,
           userId: user!.id,
         }),
@@ -151,6 +284,34 @@ export default function Addresses() {
     } catch (err) {
       setIsLoading(false);
       console.error("An error occurred: ", err);
+    }
+  }
+  async function deleteAddress(
+    event: FormEvent<HTMLFormElement>,
+    addressId: string | null | undefined
+  ) {
+    setMessage(null);
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      const res = await fetch("http://localhost:3000/api/delete-address", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ addressId: addressId }),
+      });
+
+      if (!res.ok) {
+        setMessage("An error occurred when deleting address");
+        setIsLoading(false);
+      } else {
+        update();
+        setIsLoading(false);
+        setModalShown(null);
+        setMessage(null);
+      }
+    } catch (err) {
+      setIsLoading(false);
+      console.error(err);
     }
   }
 
@@ -227,15 +388,33 @@ export default function Addresses() {
                       <p>{address.receiverPhone}</p>
                       <p>{address.fullAddress}</p>
                       <div className="flex gap-4 items-center text-sm font-semibold">
-                        <p>Change address</p>
-                        <div className="w-px h-auto bg-primary bg-opacity-10" />
                         <button
-                          onClick={() => updatePrimaryAddress(address.id!)}
+                          onClick={() => {
+                            setModalShown("update");
+                            setAddressToUpdate(address);
+                          }}
                         >
-                          Make this as a main address
+                          Change address
                         </button>
                         <div className="w-px h-auto bg-primary bg-opacity-10" />
-                        <p>Delete</p>
+                        {address.mainAddressFor !== user.id && (
+                          <>
+                            <button
+                              onClick={() => updatePrimaryAddress(address.id!)}
+                            >
+                              Make this as a main address
+                            </button>
+                            <div className="w-px h-auto bg-primary bg-opacity-10" />
+                          </>
+                        )}
+                        <button
+                          onClick={() => {
+                            setModalShown("delete");
+                            setAddressToUpdate(address);
+                          }}
+                        >
+                          Delete address
+                        </button>
                       </div>
                     </div>
                     {address.mainAddressFor === user.id && (
@@ -256,7 +435,7 @@ export default function Addresses() {
         </div>
       )}
 
-      {showModal(modalShown)}
+      {showModal(modalShown, addressToUpdate)}
     </div>
   );
 }
