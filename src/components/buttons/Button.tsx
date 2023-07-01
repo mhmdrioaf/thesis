052892dba4ec9a263bb78058336e98279b28227f
Rio Ananta <rioananta1@gmail.com>
2023-07-01 15:30:26 +0700

    `components` reusable button component

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,36 @@
+import { ButtonHTMLAttributes } from "react";
+
+type ButtonVariants = {
+  PRIMARY: string;
+  ERROR: string;
+  SECONDARY: string;
+};
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variants?: keyof ButtonVariants;
+  fullWidth?: boolean;
+  children: React.ReactNode;
+}
+
+export default function Button({
+  variants,
+  fullWidth = false,
+  children,
+  ...props
+}: ButtonProps) {
+  const buttonWidth = fullWidth ? "w-full " : "w-fit ";
+  const buttonBaseStyles =
+    buttonWidth +
+    "px-2 py-2 rounded-md grid place-items-center bg-opacity-95 hover:bg-opacity-100 disabled:bg-gray-300 disabled:text-gray-500 ";
+  const buttonVariants = {
+    PRIMARY: buttonBaseStyles + "bg-primary text-white",
+    SECONDARY: buttonBaseStyles + "bg-neutral-500 text-neutral-50",
+    ERROR: buttonBaseStyles + "bg-red-950 text-white",
+  };
+
+  return (
+    <button className={buttonVariants[variants ?? "PRIMARY"]} {...props}>
+      {children}
+    </button>
+  );
+}
