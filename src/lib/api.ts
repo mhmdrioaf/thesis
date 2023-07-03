819ed156f1abf61695369abc49f8bb73ae3aada3
Rio Ananta <rioananta1@gmail.com>
2023-07-03 15:59:19 +0700

    `fix` adding method

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,8 @@ export async function getProducts() {
   const fetchProducts = await fetch(
     process.env.NEXT_PUBLIC_API_PRODUCT_LIST_PRODUCTS!,
     {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
       next: {
         tags: ["products"],
       },
