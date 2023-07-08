9c6090334fbb64e903e0062035767f92a5fa6bd5
Rio Ananta <rioananta1@gmail.com>
2023-07-09 01:56:36 +0700

    `optimized` seller validation

--- src/app/api/(products)/create-product/route.ts
+++ src/app/api/(products)/create-product/route.ts
@@ -8,33 +8,48 @@ interface RequestBody {
   seller: string;
   thumbnail?: string;
   stock: number;
+  token: string;
 }
 
 async function handler(request: NextRequest) {
   const body: RequestBody = await request.json();
 
-  const verifySeller = await db.seller.findFirst({
-    where: {
-      id: body.seller,
-    },
-  });
-
-  if (verifySeller) {
-    const newProduct = await db.product.create({
-      data: {
-        name: body.name,
-        descriptions: body.description,
-        price: body.price,
-        sellerId: body.seller,
-        thumbnail: body.thumbnail,
-        stock: body.stock,
+  if (body.token === process.env.SELLER_TOKEN) {
+    const verifySeller = await db.seller.findFirst({
+      where: {
+        id: body.seller,
       },
     });
 
-    return new NextResponse(JSON.stringify(newProduct));
-  }
+    if (verifySeller) {
+      await db.product.create({
+        data: {
+          name: body.name,
+          descriptions: body.description,
+          price: body.price,
+          sellerId: body.seller,
+          thumbnail: body.thumbnail,
+          stock: body.stock,
+        },
+      });
 
-  return NextResponse.json({ error: "no-session" }, { status: 500 });
+      return NextResponse.json({
+        ok: true,
+        error: { code: null, message: null },
+        message: "The product has been created successfully.",
+      });
+    }
+  } else {
+    return NextResponse.json({
+      ok: false,
+      error: {
+        code: "no-access",
+        message:
+          "You have no access to create products. Either your token is invalid, or your account id is not recognized.",
+      },
+      message: null,
+    });
+  }
 }
 
 export { handler as POST };
