7d92f63cd6b4029c8b8f5a896cd1fa4841e47276
Rio Ananta <rioananta1@gmail.com>
2023-07-09 01:57:11 +0700

    user validation

--- src/app/api/(user)/delete-user/route.ts
+++ src/app/api/(user)/delete-user/route.ts
@@ -18,11 +18,36 @@ async function handler(request: NextRequest) {
     },
   });
 
+  const seller = await db.seller.findFirst({
+    where: {
+      username: body.username,
+    },
+    select: {
+      id: true,
+    },
+  });
+
   if (currentUser && body.id === currentUser.id) {
     try {
       const user = await db.customer.delete({
         where: {
           id: currentUser.id,
+        },
+      });
+
+      if (user) {
+        return NextResponse.json({ ok: true });
+      } else {
+        return NextResponse.json({ ok: false });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  } else if (seller && body.id === seller.id) {
+    try {
+      const user = await db.seller.delete({
+        where: {
+          id: seller.id,
         },
       });
 
